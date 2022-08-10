80c7448721f5809c05b1344e01c2d827fbcfb0ff
Samastul Singh <54389366+samastul@users.noreply.github.com>
2022-08-10 23:54:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const API_KEY ='api_key=d098ea827529c13f4c8053f69c7d3553';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+ API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
 const main = document.getElementById('main');
 
