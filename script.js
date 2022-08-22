fa755ca62b39b03d42d85bec0b3c850bf720ff92
Samastul Singh <54389366+samastul@users.noreply.github.com>
2022-08-22 15:50:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
 const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const myList = document.getElementsByClassName('addButton');
 
 getMovies(API_URL);
 
 function getMovies(url){
 
     fetch(url).then(response => response.json()).then(data => {
-        
+        // console.log(data.results);
         showMovies(data.results);
     })
 }
@@ -21,23 +24,52 @@ function showMovies(data){
     main.innerHTML = '';
 
     data.forEach(movie => {
-        const {title, poster_path,overview} = movie;
+        const {title, poster_path,overview,genre_ids} = movie;
         const movieTile = document.createElement('div');
         movieTile.classList.add('movie');
         movieTile.innerHTML = `
-        <img src="${IMG_URL+poster_path}" alt="${title}">
-
-            <div class="movieInfo">
-                <h3>${title}</h3>
-                
-            </div>
+        
+        <div class="poster">
+            <img src="${IMG_URL+poster_path}" alt="${title}">
             <div class="overview">
                 <h3>Overview</h3>
                 ${overview}
             </div>
-            `
+        </div>
+            <div class="movieInfo">
+                <h3>${title}</h3>
+                <div style = "display:flex; justify-content:space-between; align-items:center;">
+                    <span class="genre">${genre_ids}</span>
+                    <button class="addButton">+</button>
+                </div>
+            </div>
+
+        `
         main.appendChild(movieTile);
     });
 }
 
-{/* <span class="genre">${genre.name}</span> */}
+form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm)
+    }
+    else{
+        getMovies(API_URL)
+    }
+})
+
+myList.addEventListener('submit',(e) =>{
+    e.preventDefault();
+
+    nameTile.classList.add('movie');
+    nameTile.innerHTML = `
+        <div class = "posterIcon'
+        <img src="${IMG_URL+poster_path}" alt="e">
+    
+    
+    `
+})
